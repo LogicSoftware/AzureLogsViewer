87b3ed3930c6cc06633a6dd8626bc8f2b737ae80
denis borovnev <denis.borovnev@gmail.com>
2015-03-19 00:12:18 +0300

    added base implemenation for logs reading

---
+++ src/AzureLogsViewer.Model/Entities/WadLogEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AzureLogsViewer.Model.WadLogs;
+
+namespace AzureLogsViewer.Model.Entities
+{
+    public class WadLogEntry
+    {
+        public WadLogEntry()
+        {}
+
+        internal WadLogEntry(WadLogEntity entity)
+        {
+            PartitionKey = entity.PartitionKey;
+            RowKey = entity.RowKey;
+            Role = entity.Role;
+            RoleInstance = entity.RoleInstance;
+            Level = entity.Level;
+            Message = entity.Message;
+            Pid = entity.Pid;
+            Tid = entity.Tid;
+            EventId = entity.EventId;
+            EventDateTime = entity.EventDateTime;
+        }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string Role { get; set; }
+        public string RoleInstance { get; set; }
+        public int Level { get; set; }
+        public string Message { get; set; }
+        public int Pid { get; set; }
+        public int Tid { get; set; }
+        public int EventId { get; set; }
+        public DateTime EventDateTime { get; set; }
+    }
+}
---
+++ src/AzureLogsViewer.Model/WadLogs/WadLogsReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AzureLogsViewer.Model.Entities;
+using Microsoft.WindowsAzure.Storage;
+
+namespace AzureLogsViewer.Model.WadLogs
+{
+    public class WadLogsReader
+    {
+        private string CloudStorageConnectionString
+        {
+            get
+            {
+                //TODO: get from configuration
+                return File.ReadAllText("c:\\temp\\storagetableconn");
+            }
+        }
+
+        public List<WadLogEntry> Read(DateTime from, DateTime to)
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudStorageConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var wadLogs = tableClient.GetTableReference("WADLogsTable");
+
+            return wadLogs.CreateQuery<WadLogEntity>()
+                .Where(
+                wl => 
+                        String.Compare(wl.PartitionKey,"0" + from.Ticks.ToString()) >=0 && 
+                        String.Compare(wl.PartitionKey, "0" + to.Ticks.ToString()) < 0
+                )
+                .ToList()
+                .Select(x => new WadLogEntry(x))
+                .ToList();
+        }
+    }
+}
