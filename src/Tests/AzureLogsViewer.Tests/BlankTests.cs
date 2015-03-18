3ec6bdc1d25df60176711e9c42ea5873dafbdead
denis borovnev <denis.borovnev@gmail.com>
2015-03-18 23:15:34 +0300

    added a few blank projects

---
+++ src/Tests/AzureLogsViewer.Tests/BlankTests.cs
@@ -0,0 +1,13 @@
+﻿
+using NUnit.Framework;
+
+namespace AzureLogsViewer.Tests
+{
+    [TestFixture]
+    public class BlankTests
+    {
+        //NOTE: don't commit any changes in this file
+        //      it's just to check something instead of writing test console apps :)
+      
+    }
+}
