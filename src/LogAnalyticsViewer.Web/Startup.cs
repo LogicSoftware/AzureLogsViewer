4a2db64e76c5c17a8a7bd2174d8108d35c81c681
Alex Fomin <Alex.Fomin@logicsoftware.net>
2022-03-21 17:50:30 +0300

    Add AD authorization

--- src/LogAnalyticsViewer.Web/Startup.cs
+++ src/LogAnalyticsViewer.Web/Startup.cs
@@ -2,13 +2,16 @@ using LogAnalyticsViewer.Model;
 using LogAnalyticsViewer.Model.Services.Events;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
 
 namespace LogAnalyticsViewer.Web
 {
@@ -24,7 +27,13 @@ namespace LogAnalyticsViewer.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
+            services.AddControllersWithViews(options =>
+                {
+                    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build();
+                    options.Filters.Add(new AuthorizeFilter(policy));
+                })
                 .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
             // In production, the React files will be served from this directory
@@ -39,6 +48,9 @@ namespace LogAnalyticsViewer.Web
 
             services.AddDbContext<LAVDataContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
+            
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,12 +71,28 @@ namespace LogAnalyticsViewer.Web
             app.UseSpaStaticFiles();
 
             app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+                
+            });
+            
+            app.Use(async (context, next) =>
+            {
+                if (context.User.Identity?.IsAuthenticated == true)
+                {
+                    await next();
+                }
+                else
+                {
+                    await context.ChallengeAsync("OpenIdConnect");
+                }
             });
 
             app.UseSpa(spa =>
@@ -83,15 +111,11 @@ namespace LogAnalyticsViewer.Web
 
         private static void UpdateDatabase(IApplicationBuilder app)
         {
-            using (var serviceScope = app.ApplicationServices
+            using var serviceScope = app.ApplicationServices
                 .GetRequiredService<IServiceScopeFactory>()
-                .CreateScope())
-            {
-                using (var context = serviceScope.ServiceProvider.GetService<LAVDataContext>())
-                {
-                    context.Database.Migrate();
-                }
-            }
+                .CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<LAVDataContext>();
+            context.Database.Migrate();
         }
     }
 }
